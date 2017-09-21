15e5503a5995970ce91d0a7f2da4ce1bb5de26bb
Merlin Sudeepti <merlin.sudeepti@arction.com>
2017-09-21 09:36:02 +0200

    Async array fill example added

--- year.js
+++ year.js
@@ -1,11 +1,14 @@
-//function generate_year_range(start, end){
-    //var years = [];
-    //for(var year = start; year <= end; year++){
-      //  years.push(year)
-    //}
-    //return years;
-//}
-//var my_years = generate_year_range(2000,2009);
-//console.log(my_years);
+function generate_year_range(start, end){
+  var years = [];
+   for(var year = start; year <= end; year++){
+        years.push(year)
+    }
+  return years;
+}
+var my_years = generate_year_range(2000,2009);
+console.log(my_years);
+
+
+
 
 
